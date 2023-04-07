eba30b3a749bcbc53996f4f595dc96e06eca22e5
Valera <guest19971220@gmail.com>
2023-04-07 11:24:04 +0800

    final

--- lhnightmarket.js
+++ lhnightmarket.js
@@ -10,3 +10,14 @@ document.querySelector('#close-share').onclick = () =>{
     share.classList.remove('active');
 }
 //分享 end//
+
+//navbar start//
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+
+menu.onclick = ()=>{
+    menu.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+//navbar end//
